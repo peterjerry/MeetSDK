7867efc4ddf9f696baec6037c6ed089cf2630f06
Michael.Ma <guoliangma@pptv.com>
2015-07-31 13:28:46 +0800

    meetsdk catch IllegalStateException

--- android/MeetSDK/src/android/pplive/media/player/MeetVideoView.java
+++ android/MeetSDK/src/android/pplive/media/player/MeetVideoView.java
@@ -297,6 +297,13 @@ public class MeetVideoView extends SurfaceView implements MediaPlayerControl {
             mTargetState = STATE_ERROR;
             mErrorListener.onError(mMediaPlayer, MediaPlayer.MEDIA_ERROR_UNKNOWN, 0);
             return;
+        } catch (IllegalStateException ex) {
+        	LogUtils.error("Unable to open content(IllegalStateException): " + mUri);
+            Log.e(TAG, "Unable to open content: " + mUri, ex);
+            mCurrentState = STATE_ERROR;
+            mTargetState = STATE_ERROR;
+            mErrorListener.onError(mMediaPlayer, MediaPlayer.MEDIA_ERROR_UNKNOWN, 0);
+            return;
         }
     }
     
