aaa5b37141c13933961de0d1f5b5c6f799e913ca
michael.ma <guoliangma@pptv.com>
2015-09-18 12:19:26 +0800

    change iOS meet player ui

--- ios/workspace2/MeetPlayer/MeetPlayer/PlaybackViewController.m
+++ ios/workspace2/MeetPlayer/MeetPlayer/PlaybackViewController.m
@@ -141,12 +141,12 @@
 
 - (void)displayMovieMessage
 {
-    NSString *msg = [NSString stringWithFormat:@"Type: %@", [player_ playerType]==PPMOVIE_SYSTEM_PLAYER ? @"system" : @"ppplayer"];
+    NSString *msg = [NSString stringWithFormat:@"Type: %@", [player_ playerType]==PPMOVIE_SYSTEM_PLAYER ? @"system player" : @"pp player"];
     //NSString *msg1 = [NSString stringWithFormat:@"Url: %@", [url_ absoluteString]];
-    NSString *msg2 = [NSString stringWithFormat:@"Duration: %f", player_.duration];
-    NSString *msg3 = [NSString stringWithFormat:@"Size: %f * %f", player_.size.width, player_.size.height];
-    NSString *msg4 = [NSString stringWithFormat:@"numberOfBytesTransfered: %lld", [player_ numberOfBytesTransfered]];
-    NSString *msg5 = [NSString stringWithFormat:@"buffering time: %f", player_.bufferingTime];
+    NSString *msg2 = [NSString stringWithFormat:@"Duration: %.3f sec", player_.duration];
+    NSString *msg3 = [NSString stringWithFormat:@"Size: %.0f * %.0f", player_.size.width, player_.size.height];
+    NSString *msg4 = [NSString stringWithFormat:@"bytes trans: %lld", [player_ numberOfBytesTransfered]];
+    NSString *msg5 = [NSString stringWithFormat:@"buffering time: %.0f", player_.bufferingTime];
     NSString *tmp = [NSString stringWithFormat:@"%@\r\n%@\r\n%@\r\n%@\r\n%@", msg, msg2, msg3, msg4, msg5];
     messageLab_.text = tmp;
 }
