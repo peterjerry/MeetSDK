71e980503d6a3e1eb22c7f6bdc99ccff68863cd6
Michael.Ma <michael@MichaelMas-MacBook-Pro.local>
2016-01-25 10:32:30 +0800

    fix ios build problem

--- foundation/build/ios/config_xc6.sh
+++ foundation/build/ios/config_xc6.sh
@@ -69,21 +69,21 @@ esac
 #if [[ $2 = 'lite' ]]
 if [ ${2}x == 'lite'x ]
 then
-echo "lite build"
-EXTRA_PARAMETERS="$EXTRA_PARAMETERS \
-	--disable-decoders \
-	--enable-decoder=h263,h264,hevc,vp3,vp5,vp6,vp6a,vp6f,vp7,vp8,vp9,flv,mpeg1video,mpeg2video,mpegvideo,mpeg4,dca,aac,mp1,mp2,mp3,rv30,rv40,cook,wmv1,wmv2,wmv3,wmv3image,vorbis,ape,flac,wmav1,wmav2,wmapro,mjpeg,msmpeg4v1,msmpeg4v2,msmpeg4v3,tscc,amrnb,amrwb,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,ass,dvbsub,dvdsub,mov_text,sami,srt,ssa,subrip,text \
-	--disable-demuxers \
-	--enable-demuxer=rm,mpegvideo,mjpeg,avi,h263,h264,hevc,matroska,dts,dtshd,aac,flv,mpegts,mpegps,mp4,m4v,mov,ape,hls,flac,rawvideo,realtext,rtsp,vc1,mp3,wav,asf,ogg \
-	--disable-parsers \
-	--enable-parser=h263,h264,hevc,mpegaudio,mpegvideo,aac_latm,mpeg4video,dca,aac,flac,png,bmp,rv30,rv40,cavsvideo,vc1,vorbis,mjpeg,vp3,vp8,vp9,cook \
-	--enable-openssl"
-EXTRA_C_FLAGS="$EXTRA_C_FLAGS -I$OPENSSL_INCLUDE"
-EXTRA_LD_FLAGS="$EXTRA_LD_FLAGS -L$OPENSSL_LIB -lssl -lcrypto -lz"
-echo "EXTRA_C_FLAGS ${EXTRA_C_FLAGS}"
-echo "EXTRA_LD_FLAGS ${EXTRA_LD_FLAGS}"
+	echo "lite build"
+	EXTRA_PARAMETERS="$EXTRA_PARAMETERS \
+		--disable-decoders \
+		--enable-decoder=h263,h264,hevc,vp3,vp5,vp6,vp6a,vp6f,vp7,vp8,vp9,flv,mpeg1video,mpeg2video,mpegvideo,mpeg4,dca,aac,mp1,mp2,mp3,rv30,rv40,cook,wmv1,wmv2,wmv3,wmv3image,vorbis,ape,flac,wmav1,wmav2,wmapro,mjpeg,msmpeg4v1,msmpeg4v2,msmpeg4v3,tscc,amrnb,amrwb,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,ass,dvbsub,dvdsub,mov_text,sami,srt,ssa,subrip,text \
+		--disable-demuxers \
+		--enable-demuxer=rm,mpegvideo,mjpeg,avi,h263,h264,hevc,matroska,dts,dtshd,aac,flv,mpegts,mpegps,mp4,m4v,mov,ape,hls,flac,rawvideo,realtext,rtsp,vc1,mp3,wav,asf,ogg \
+		--disable-parsers \
+		--enable-parser=h263,h264,hevc,mpegaudio,mpegvideo,aac_latm,mpeg4video,dca,aac,flac,png,bmp,rv30,rv40,cavsvideo,vc1,vorbis,mjpeg,vp3,vp8,vp9,cook"
+#		--enable-openssl"
+#	EXTRA_C_FLAGS="$EXTRA_C_FLAGS -I$OPENSSL_INCLUDE"
+#	EXTRA_LD_FLAGS="$EXTRA_LD_FLAGS -L$OPENSSL_LIB -lssl -lcrypto -lz"
+#	echo "EXTRA_C_FLAGS ${EXTRA_C_FLAGS}"
+#	echo "EXTRA_LD_FLAGS ${EXTRA_LD_FLAGS}"
 else
-echo "full build"
+	echo "full build"
 fi
 
 #ios disble ac3
