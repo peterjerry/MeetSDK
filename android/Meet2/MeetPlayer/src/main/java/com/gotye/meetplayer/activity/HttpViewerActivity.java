59065fe1ebc12dc22530e9121f9e1db684545c02
Michael.Ma <wnpllr@gmail.com>
2016-12-15 14:51:29 +0800

    1 foundation: update thirdparty/x264 from 146 to 148 2 MeetPlayer: fix HttpViewerActivity.java

--- android/Meet2/MeetPlayer/src/main/java/com/gotye/meetplayer/activity/HttpViewerActivity.java
+++ android/Meet2/MeetPlayer/src/main/java/com/gotye/meetplayer/activity/HttpViewerActivity.java
@@ -94,11 +94,12 @@ public class HttpViewerActivity extends AppCompatActivity {
                         url.endsWith("ts") || url.endsWith("avi") ||
                         url.endsWith("ape") || url.endsWith("flac") ||
                         url.endsWith("mov") || url.endsWith("mkv") ||
-                        url.endsWith("wav") ||
+                        url.endsWith("rmvb") || url.endsWith("rm") ||
+                        url.endsWith("wav") || url.endsWith("m3u8") ||
                         url.startsWith("rtmp://") || url.startsWith("rtsp://")) {
                     Intent intent = new Intent(HttpViewerActivity.this, VideoPlayerActivity.class);
                     intent.setData(Uri.parse(url));
-                    intent.putExtra("player_impl", mPlayerImpl);
+                    intent.putExtra("impl", mPlayerImpl);
                     intent.putExtra("title", "N/A");
                     startActivity(intent);
                     return true;
